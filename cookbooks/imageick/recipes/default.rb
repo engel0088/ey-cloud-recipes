efac9dd6d76557364fe237ad327a326702ed00cb
engel lee <engel0088@gmail.com>
2013-07-12 15:24:47 +0800

    version 6.4.9.2

--- cookbooks/imageick/recipes/default.rb
+++ cookbooks/imageick/recipes/default.rb
@@ -4,10 +4,10 @@
 #
 
 enable_package "media-gfx/imagemagick" do
-  version "6.6.9.7"
+  version "6.4.9.2"
 end
 
 package "media-gfx/imagemagick" do
-  version "6.6.9.7"
+  version "6.4.9.2"
   action :install
 end
