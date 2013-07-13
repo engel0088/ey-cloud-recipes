556935632c5129d9c8519eae2c9cde2bc0b85769
engel lee <engel0088@gmail.com>
2013-07-13 09:39:59 +0800

    update imagemagick version, and extra package

--- cookbooks/imageick/recipes/default.rb
+++ cookbooks/imageick/recipes/default.rb
@@ -3,11 +3,15 @@
 # Recipe:: default
 #
 
+enable_package "media-libs/lcms" do
+  version "2.3 ~amd64"
+end
+
 enable_package "media-gfx/imagemagick" do
-  version "6.4.9.2"
+  version "6.7.8.8-r1"
 end
 
 package "media-gfx/imagemagick" do
-  version "6.4.9.2"
+  version "6.7.8.8-r1"
   action :install
 end
